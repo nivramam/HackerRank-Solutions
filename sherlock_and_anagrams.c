749a458bc13bc034485c80c6f3ee2e437e1667d7
Niveydhithaa. R <36819505+nivramam@users.noreply.github.com>
2019-12-08 22:23:46 +0530

    Create sherlock_and_anagrams.c

---
+++ sherlock_and_anagrams.c
@@ -0,0 +1,21 @@
+int check_anagram(char a[], char b[])
+{
+   int first[26] = {0}, second[26] = {0}, c = 0;
+
+   while (a[c] != '\0') {
+      first[a[c]-'a']++;
+      c++;
+   }
+   c = 0;
+   while (b[c] != '\0') {
+      second[b[c]-'a']++;
+      c++;
+   }
+
+   for (c = 0; c < 26; c++) {
+      if (first[c] != second[c])
+         return 0;
+   }
+
+   return 1;
+}
